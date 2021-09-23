d3942d8df4723d20d6b359732a485491aed8e2e8
Gerryl <GerrylEsperacion@gmail.com>
2021-09-22 18:28:29 -0700

    removed tag_notes associations

--- app/models/tag_note.py
+++ app/models/tag_note.py
@@ -1,9 +1,16 @@
-from .db import db
+# from .db import db
 
 
-class Tag_note(db.Model):
-    __tablename__ = 'tag_notes'
+# tag_notes = db.table(
+#     "tag_notes",
+#     db.Model.metadata,
+#     db.Column("tags_id", db.Integer, db.ForeignKey('tags.id'), primary_key=True),
+#     db.Column("notes_id", db.Integer, db.ForeignKey('notes.id'), primary_key=True),
+# )
 
-    id = db.Column(db.Integer, primary_key=True)
-    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
-    note_id = db.Column(db.Integer, db.ForeignKey('note.id'))
+# class Tag_note(db.Model):
+#     __tablename__ = 'tag_notes'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'))
+#     note_id = db.Column(db.Integer, db.ForeignKey('notes.id'))
